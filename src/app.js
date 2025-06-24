faa306f54cb17dec87cd7f3c6261b7a4c53ce7aa
CarolinaNFeria <rogua20.12@gmail.com>
2025-06-24 16:31:25 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+  let sujetos = ["Mi perro", "El vecino", "Mi novio", "Mi abuela"];
+  let acciones = ["se comió", "rompió", "perdió", "escondió"];
+  let que = ["mis deberes", "las llaves", "el informe", "mi teléfono"];
+  let circunstancias = ["justo antes de venir","mientras dormía","cuando estaba en el baño","en medio de la tormenta"];
+ 
+function numeroAleatorio (a,b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function esAleatorio(AnyArray) {
+  let max = AnyArray.length = 4;
+  let min = 0;
+  console.log(min, max, AnyArray)
+  let Aleatorio = numeroAleatorio(min,max);
+  return AnyArray[Aleatorio];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log();
+document.querySelector("#titulo").innerHTML = esAleatorio(sujetos) + " " + esAleatorio(acciones) + " " + esAleatorio(que) + " " + esAleatorio(circunstancias);
 };
